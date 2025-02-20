da157febb222994dc5bf0920403c385baea4ff91
Mihai Costea <macostea@users.noreply.github.com>
2025-02-20 13:24:49 +0000

    Skeleton

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", async (req, res) => {
+  res.send("Hello world! This is a change!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
