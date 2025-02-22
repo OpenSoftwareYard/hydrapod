2e7f880d1862dc1212235e11463a8e88a5945756
Mihai Costea <macostea@users.noreply.github.com>
2025-02-22 10:20:15 +0200

    Omit connectionKey from node response

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 import { fieldEncryptionExtension } from "prisma-field-encryption";
 
-const globalClient = new PrismaClient();
+const globalClient = new PrismaClient({
+  omit: {
+    node: {
+      connectionKey: true,
+    },
+  },
+});
 
 export const client = globalClient.$extends(fieldEncryptionExtension());
 
