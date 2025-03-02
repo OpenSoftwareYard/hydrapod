cf2a675d0bb9002767207506491077008113f459
Mihai Costea <macostea@users.noreply.github.com>
2025-03-02 17:01:22 +0200

    Add more connector functions

--- src/node-connector.ts
+++ src/node-connector.ts
@@ -2,6 +2,7 @@ import * as fs from "fs";
 import * as path from "path";
 import type { Node } from "@prisma/client";
 import { NodeSSH } from "node-ssh";
+import { Vnic } from "./types";
 
 export class NodeConnector {
   constructor() {}
@@ -24,10 +25,34 @@ export class NodeConnector {
       "utf-8",
     );
     const result = await ssh.execCommand(getZonesScript);
-    console.log(result.stdout);
 
     ssh.dispose();
 
+    if (result.code !== 0) {
+      throw new Error(result.stderr);
+    }
+
     return JSON.parse(result.stdout);
   }
+
+  async getZone(node: Node, zoneId: string) {
+    const ssh = await this.connect(node);
+    const getZoneScript = await fs.promises.readFile(
+      path.join(__dirname, "..", "scripts", "GetZone.sh"),
+      "utf-8",
+    );
+    const result = await ssh.execCommand(
+      getZoneScript.replaceAll("$1", zoneId),
+    );
+
+    ssh.dispose();
+
+    if (result.code !== 0) {
+      throw new Error(result.stderr);
+    }
+
+    return JSON.parse(result.stdout);
+  }
+
+  async createVnic(node: Node, vnic: Vnic) {}
 }
--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,16 @@ export enum ZoneStatus {
   Stopped = "stopped",
   Scheduling = "scheduling",
 }
+
+export type Vnic = {
+  link: string;
+  over: string;
+  speed?: string;
+  macAddress?: string;
+  macAddressType?: string;
+  vid?: string;
+};
+
+export const shortenLinkName = (longLinkName: string) => {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+};
