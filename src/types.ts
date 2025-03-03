e9ed5dc41f5d48f1036677e3d3194db496a0d9e1
Mihai Costea <macostea@users.noreply.github.com>
2025-03-03 13:19:28 +0000

    Remove old implementation

--- src/types.ts
+++ src/types.ts
@@ -4,33 +4,3 @@ export enum ZoneStatus {
   Stopped = "stopped",
   Scheduling = "scheduling",
 }
-
-export type Vnic = {
-  link: string;
-  over: string;
-  speed?: string;
-  macAddress?: string;
-  macAddressType?: string;
-  vid?: string;
-};
-
-export const shortenLinkName = (longLinkName: string) => {
-  const alphabet = "abcdefghijklmnopqrstuvwxyz";
-
-  let hash = 0;
-  for (let i = 0; i < longLinkName.length; i++) {
-    hash = (hash << 5) - hash + longLinkName.charCodeAt(i);
-    hash = hash & hash;
-  }
-
-  let result = "";
-  let seed = Math.abs(hash);
-
-  for (let i = 0; i < 6; i++) {
-    seed = (seed * 1664525 + 1013904223) >>> 0;
-    const letterIndex = seed % alphabet.length;
-    result += alphabet[letterIndex];
-  }
-
-  return result + "0";
-};
